81f592e62324e9be54fa2265556249d5c658d940
Thomas Halstead <tom.michael.halstead@gmail.com>
2025-01-28 01:06:48 -0600

    Back for real this time, coming out of a little depression and had a lot going on'

---
+++ src/main/java/config/WebConfiguration.java
@@ -0,0 +1,12 @@
+package config;
+
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+    }
+}
